3807fa5d41b584b7fc5b66db678b584a3f26f727
Matheus Alves <matheus.aalves@hotmail.com>
2021-10-28 10:24:20 -0300

    Resolve requisito 5

--- script.js
+++ script.js
@@ -4,14 +4,38 @@ const cartaGerada = document.querySelector('#carta-gerada');
 
 const classes = ['newspaper', 'magazine1', 'magazine2', 'medium', 'big',
   'reallybig', 'rotateleft', 'rotateright', 'skewleft', 'skewright'];
+// Source: https://stackoverflow.com/questions/4550505/getting-a-random-value-from-a-javascript-array
+
+// function addItem() {
+//   const pegaConteudo = document.createElement('span');
+//   if (!inputTexto.value || !inputTexto.value.trim()) { // Source: https://stackoverflow.com/questions/33682536/how-can-trim-spaces-in-all-inputs-without-adding-methods-or-classes
+//     cartaGerada.innerText = 'Por favor, digite o conteúdo da carta.';
+//   } else {
+//     pegaConteudo.innerText = inputTexto.value.split(' ');
+//     const array = pegaConteudo.innerText;
+//     console.log(array);
+//     const randomElement = classes[Math.floor(Math.random() * classes.length)]; // gera aleatoriamente uma classe do array classes
+//     pegaConteudo.className = randomElement;
+//   }
+//   cartaGerada.appendChild(pegaConteudo);
+// }
+// btnCriarCarta.addEventListener('click', addItem);
 
 function addItem() {
-  const pegaConteudo = document.createElement('p');
-  pegaConteudo.innerText = inputTexto.value;
-  const randomElement = classes[Math.floor(Math.random() * classes.length)]; // gera aleatoriamente uma classe do array classes
-  // Source: https://stackoverflow.com/questions/4550505/getting-a-random-value-from-a-javascript-array
-  pegaConteudo.className = randomElement;
-  cartaGerada.appendChild(pegaConteudo);
+  const pegaConteudo = document.createElement('span');
+  if (!inputTexto.value || !inputTexto.value.trim()) { // Source: https://stackoverflow.com/questions/33682536/how-can-trim-spaces-in-all-inputs-without-adding-methods-or-classes
+    cartaGerada.innerText = 'Por favor, digite o conteúdo da carta.';
+    cartaGerada.appendChild(pegaConteudo);
+  } else {
+    pegaConteudo.innerText = inputTexto.value.split(' ');
+    const array = pegaConteudo.innerText;
+    for (let index = 0; index < array.length; index += 1) {
+      console.log(array);
+      const randomElement = classes[Math.floor(Math.random() * classes.length)]; // gera aleatoriamente uma classe do array classes
+      array[index].className = randomElement;
+      cartaGerada.appendChild(pegaConteudo);
+    }
+  }
+  
 }
-
 btnCriarCarta.addEventListener('click', addItem);
